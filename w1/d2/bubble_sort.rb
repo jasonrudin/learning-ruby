41bf5b5b336710abe576216a9caa55cb9eb15c60
Jason Rudin <jrudin@jrudin-macbookpro.roam.corp.google.com>
2017-08-23 09:04:46 -0700

    bubble sort in progress

--- w1/d2/bubble_sort.rb
+++ w1/d2/bubble_sort.rb
@@ -1,6 +1,18 @@
 def bubble_sort(unsortedArray)
-	unsortedArray.each_with_index do |element, i|
-		p "#{element}, #{i}"
+	hasBeenSwapped = true
+	n = unsortedArray.length
+	while hasBeenSwapped
+		hasBeenSwapped = false
+		#(n-1).times do |element, i|
+			if element > unsortedArray[i+1]
+				p "#{element}, #{unsortedArray[i+1]}"
+				temp = element
+				unsortedArray[i] = unsortedArray[i+1]
+				unsortedArray[i+1] = temp
+				hasBeenSwapped = true
+				p unsortedArray
+			end
+		end
 	end
 
 end
