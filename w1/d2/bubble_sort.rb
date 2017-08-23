8f969264e85d633d8f39fd25a12e6fd388837857
Jason Rudin <jrudin@jrudin-macbookpro.roam.corp.google.com>
2017-08-23 09:13:58 -0700

    bubble sort finished. Couldn't modify an array while I was iterating over it, so I had to switch to a counter and keep track of the index manually.

--- w1/d2/bubble_sort.rb
+++ w1/d2/bubble_sort.rb
@@ -1,20 +1,23 @@
 def bubble_sort(unsortedArray)
 	hasBeenSwapped = true
 	n = unsortedArray.length
+
 	while hasBeenSwapped
 		hasBeenSwapped = false
-		#(n-1).times do |element, i|
-			if element > unsortedArray[i+1]
-				p "#{element}, #{unsortedArray[i+1]}"
-				temp = element
+		i = 0
+		(n-1).times do
+			if unsortedArray[i] > unsortedArray[i+1]
+				p "#{unsortedArray[i]}, #{unsortedArray[i+1]}"
+				temp = unsortedArray[i]
 				unsortedArray[i] = unsortedArray[i+1]
 				unsortedArray[i+1] = temp
 				hasBeenSwapped = true
 				p unsortedArray
 			end
+			i = i + 1
 		end
 	end
 
 end
 
-bubble_sort([3,7,2,-1,0,2,10,4])
+bubble_sort([3,-5,7,2,-1,0,2,10,4])
