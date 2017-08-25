9b292353835757c15f5a1cbaf86a034dbecf3d46
Jason Rudin <jasonrudin@Jasons-MacBook-Pro.local>
2017-08-25 08:16:53 -0700

    Mastermind in progress, still to add storage of all past guesses and display them for easier gameplay.

--- w1/d3/mastermind.rb
+++ w1/d3/mastermind.rb
@@ -2,11 +2,14 @@ class MasterMind
 
 	def initialize
 		@answer = setWinningAnswer
+		@number_of_turns = 10
+		@previousAnswers = []
 		self.run
 	end
 
 	def setWinningAnswer
-		numColorHash = {0 => "R", 1=> "G", 2 => "B", 3 => "Y", 4=> "O", 5 => "P"}
+		numColorHash = {0 => "R", 1=> "G", 
+			2 => "B", 3 => "Y", 4=> "O", 5 => "P"}
 		working_answer = []
 		4.times do
 			working_answer << numColorHash[rand(6)]
@@ -15,15 +18,28 @@ class MasterMind
 	end
 
 	def run
-		p "Your choice please:"
 		response = ""
-		while !validateResponse(response)
-			response = gets.chomp
+		p @answer
+		while @number_of_turns > 0
+			while !checkIfValidResponse(response)
+				puts "Your choice please:"
+				response = gets.chomp
+			end
+			if checkResponse(response)
+				winningMessage
+				break
+			else
+				incorrectResponseMessage
+			end
+			@number_of_turns = @number_of_turns - 1
+			response = ""
 		end
-		p response
+		if @number_of_turns == 0
+			losingMessage
+		end
 	end
 
-	def validateResponse(response)
+	def checkIfValidResponse(response)
 		if response.length != 4
 			false
 		elsif (response =~ /[RGBYOP]{4}/).nil?
@@ -33,9 +49,29 @@ class MasterMind
 		end			
 	end
 
+	def checkResponse(response)
+		responseArray = response.chars
+		if(responseArray == @answer)
+			true
+		else
+			false
+		end
+	end
 
+	def winningMessage
+		puts "Congrats, you won!"
+	end
+
+	def incorrectResponseMessage
+		puts "Not quite, you have #{@number_of_turns - 1} turns left!"
+		puts "You got XX matches right and YY near matches"
+		puts "Here's what you've done so far:"
+	end
+
+	def losingMessage
+		puts "You lost, try again soon!"
+	end
 
 end
 
 MasterMind.new
-
