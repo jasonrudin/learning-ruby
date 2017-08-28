48df54514b6db84c854a33aad3f8c1c94bb0809f
Jason Rudin <jasonrudin@Jasons-MacBook-Pro.local>
2017-08-27 17:19:17 -0700

    Finished mastermind

--- w1/d3/mastermind.rb
+++ w1/d3/mastermind.rb
@@ -1,9 +1,12 @@
 class MasterMind
 
 	def initialize
+		@number_of_pegs = 4
 		@answer = setWinningAnswer
 		@number_of_turns = 10
 		@previousAnswers = []
+		@number_correct = 0
+		@number_close = 0
 		self.run
 	end
 
@@ -11,7 +14,7 @@ class MasterMind
 		numColorHash = {0 => "R", 1=> "G", 
 			2 => "B", 3 => "Y", 4=> "O", 5 => "P"}
 		working_answer = []
-		4.times do
+		@number_of_pegs.times do
 			working_answer << numColorHash[rand(6)]
 		end
 		working_answer
@@ -19,17 +22,18 @@ class MasterMind
 
 	def run
 		response = ""
-		p @answer
 		while @number_of_turns > 0
 			while !checkIfValidResponse(response)
 				puts "Your choice please:"
 				response = gets.chomp
 			end
+			@number_correct = 0
+			@number_close = 0
 			if checkResponse(response)
 				winningMessage
 				break
 			else
-				incorrectResponseMessage
+				incorrectResponseMessage(response)
 			end
 			@number_of_turns = @number_of_turns - 1
 			response = ""
@@ -40,9 +44,9 @@ class MasterMind
 	end
 
 	def checkIfValidResponse(response)
-		if response.length != 4
+		if response.length != @number_of_pegs
 			false
-		elsif (response =~ /[RGBYOP]{4}/).nil?
+		elsif (response =~ /[RGBYOP]{#{@number_of_pegs}}/).nil?
 			false
 		else
 			true
@@ -54,6 +58,20 @@ class MasterMind
 		if(responseArray == @answer)
 			true
 		else
+			tracker_array = Array.new(@number_of_pegs){ |i| 0}
+			@answer.each_with_index do |element, i|
+
+				if(element == responseArray[i])
+					@number_correct = @number_correct + 1
+					tracker_array[@answer.find_index(element)] = 1
+				elsif (responseArray.include?(element))
+					p tracker_array[@answer.find_index(element)]
+					if(tracker_array[@answer.find_index(element)] == 0)
+						@number_close = @number_close + 1
+						tracker_array[@answer.find_index(element)] = 1
+					end
+				end
+			end
 			false
 		end
 	end
@@ -62,10 +80,13 @@ class MasterMind
 		puts "Congrats, you won!"
 	end
 
-	def incorrectResponseMessage
-		puts "Not quite, you have #{@number_of_turns - 1} turns left!"
-		puts "You got XX matches right and YY near matches"
+	def incorrectResponseMessage(response)
+		@previousAnswers << "Response: #{response}, Number right: #{@number_correct}, Number close: #{@number_close}"
+		puts "Not quite, you have #{@number_of_turns - 1} turns left."
+		puts "You got #{@number_correct} matches right and #{@number_close} near matches."
 		puts "Here's what you've done so far:"
+
+		puts @previousAnswers
 	end
 
 	def losingMessage
